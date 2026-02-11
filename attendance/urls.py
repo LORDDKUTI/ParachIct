cea22e6e6d3dd52b0ff80d7768d67b6bd0946c54
KingSeraph07 <runsewemichael93@gmail.com>
2026-02-11 13:52:39 +0100

    pulled

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import login, signupStudent, studentProfile
 
 urlpatterns = [
     path('scan/', views.scan_qr, name='scan_qr'),
@@ -9,8 +8,6 @@ urlpatterns = [
 
     path('admin/courses/', views.add_course, name='add_course'),
     path('admin/locations/', views.add_location, name='add_location'),
-
-    path("signup/", signupStudent, name="signup"),
-    path("login/", login, name="login"),
-    path("student_profile", studentProfile, name="student_profile") 
+    
+    path('register/', views.student_register, name='student_register'),
 ]
